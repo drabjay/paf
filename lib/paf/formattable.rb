0292d569970685013feded7adef702e8c2f1dd85
John Bard <johnbard@globalnet.co.uk>
2017-07-07 11:36:10 +0100

    Allow formatter to be specified

--- lib/paf/formattable.rb
+++ lib/paf/formattable.rb
@@ -7,7 +7,10 @@ require 'paf/string_formatter'
 class Paf
   # Processing to format a PAF entry
   module Formattable
+    attr_accessor :formatter
+
     def self.included(base)
+      base.prepend Initializer
       base.extend ClassMethods
       base.class_eval do
         include Premises
@@ -15,6 +18,14 @@ class Paf
       end
     end
     private_class_method :included
+
+    # initialize method to be prepended to the including class
+    module Initializer
+      def initialize(*)
+        super if defined? super
+        @formatter ||= Paf::ArrayFormatter.new
+      end
+    end
 
     # Methods to be added to the including class
     module ClassMethods
@@ -34,7 +45,10 @@ class Paf
         new(args[0]).to_s
       end
 
-      alias format to_a
+      # Formats a hash of PAF address elements
+      def format(args)
+        new(args).format
+      end
     end
 
     # Formats a Paf instance into an array of strings
@@ -52,6 +66,9 @@ class Paf
       Paf::StringFormatter.format(self)
     end
 
-    alias format to_a
+    # Formats a Paf instance
+    def format
+      formatter.format(self)
+    end
   end
 end
---
+++ spec/paf/formatter_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+# rubocop:disable Metrics/BlockLength
+RSpec.describe Paf do
+  context 'with default formatter' do
+    let(:address) do
+      {
+        building_name: '1-2',
+        thoroughfare_name: 'NURSERY',
+        thoroughfare_descriptor: 'LANE',
+        dependent_locality: 'PENN',
+        post_town: 'HIGH WYCOMBE',
+        postcode: 'HP10 8LS'
+      }
+    end
+    let(:format) { ['1-2 NURSERY LANE', 'PENN', 'HIGH WYCOMBE', 'HP10 8LS'] }
+
+    it_behaves_like 'it is class formattable'
+    it_behaves_like 'it is instance formattable'
+  end
+
+  context 'with explicit formatter' do
+    let(:address) do
+      {
+        building_name: '1-2',
+        thoroughfare_name: 'NURSERY',
+        thoroughfare_descriptor: 'LANE',
+        dependent_locality: 'PENN',
+        post_town: 'HIGH WYCOMBE',
+        postcode: 'HP10 8LS',
+        formatter: Paf::StringFormatter.new
+      }
+    end
+    let(:format) { '1-2 NURSERY LANE, PENN, HIGH WYCOMBE. HP10 8LS' }
+
+    it_behaves_like 'it is class formattable'
+    it_behaves_like 'it is instance formattable'
+  end
+end
+# rubocop:enable Metrics/BlockLength
